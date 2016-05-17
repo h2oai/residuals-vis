f4bc8a0d5f685dae2aecb54bd17e7ce0c2b715c1
Micah Stubbs <micah.stubbs@gmail.com>
2016-05-16 19:07:02 -0700

    generalise variable names in parseResponse

--- scatterplot-zoom/11/src/parseResponse.js
+++ scatterplot-zoom/11/src/parseResponse.js
@@ -3,17 +3,17 @@ export function parseResponse(response) {
   console.log('responseData', responseData);
 
   const columnsData = responseData.frames[0].columns;
-  const exemplarMembers = [];
+  const points = [];
   columnsData.forEach(d => {
     d.data.forEach((e, j) => {
-      if (typeof exemplarMembers[j] === 'undefined') exemplarMembers[j] = {};
-      exemplarMembers[j][d.label] = e;
+      if (typeof points[j] === 'undefined') points[j] = {};
+      points[j][d.label] = e;
     });
   });
   console.log('columnsData', columnsData);
 
-  const detailData = exemplarMembers;
+  const parsedData = points;
   // console.log('exemplar', exemplar);
-  console.log('detailData', detailData);
-  return detailData;
+  console.log('parsedData', parsedData);
+  return parsedData;
 }
