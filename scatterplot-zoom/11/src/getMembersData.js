5d07b738895d329a77e99027c516e3115c7b70cb
Micah Stubbs <micah.stubbs@gmail.com>
2016-05-18 16:10:07 -0700

    use d3.queue to wait for all API calls to complete before proceeding

--- scatterplot-zoom/11/src/getMembersData.js
+++ scatterplot-zoom/11/src/getMembersData.js
@@ -2,10 +2,9 @@ import { parseResponse } from './parseResponse';
 import { drawMemberCircles } from './drawMemberCircles';
 import d3 from 'd3';
 import d3_request from 'd3-request';
+import d3_queue from 'd3-queue';
 d3.request = d3_request.request;
-
-// console.log('d3_request', d3_request);
-// console.log('d3.request', d3.request);
+d3.queue = d3_queue.queue;
 
 export function getMembersData(vis) {
   // call the h2o-3 API to get members data
@@ -19,7 +18,7 @@ export function getMembersData(vis) {
   const columnOffset = vis.apiConfig.columnOffset;
   const columnCount = vis.apiConfig.columnCount;
 
-  const queryUrl = `http://${server}:${port}/3/Frames/${membersFrame}?column_offset=${columnOffset}&column_count=${columnCount}`;
+  const getMemberFrameUrl = `http://${server}:${port}/3/Frames/${membersFrame}?column_offset=${columnOffset}&column_count=${columnCount}`;
 
   function callback(error, response) {
     console.log('response', response);
@@ -27,14 +26,13 @@ export function getMembersData(vis) {
     drawMemberCircles(vis);
   }
 
-  d3.request(queryUrl)
-    .get(callback);
-
-  /*
-  d3.xhr(queryUrl, 'application/json', (error, response) => {
-    console.log('response', response);
-    vis.detailData = parseResponse(response);
-    drawMemberCircles(vis);
-  });
-  */
+  d3.queue()
+    .defer(d3.request, getMemberFrameUrl)
+    // .defer(d3.request, "http://www.google.com:81")
+    // .defer(d3.request, "http://www.google.com:81")
+    .awaitAll((error, results) => {
+      if (error) throw error;
+      console.log(results);
+      callback(error, results[0]);
+    });
 }
