18f8ae9ed6999e6616aee4487481e5ab9f3e31d8
Micah Stubbs <micah.stubbs@gmail.com>
2016-05-18 21:42:03 -0700

    add working API call to generate membersFrame

--- scatterplot-zoom/commands.sh
+++ scatterplot-zoom/commands.sh
@@ -6,7 +6,7 @@ cd h2o-3.9.1.3469/
 java -Xmx10g -jar ./h2o.jar -port 55555 1> h2o.out 2> h2o.err &
 
 # @markc
-java -Xmx10g -jar ./h2o.jar -port 54321 1> h2o.out 2> h2o.err &
+# java -Xmx10g -jar ./h2o.jar -port 54321 1> h2o.out 2> h2o.err &
 
 # then we can open h2o flow at this url:
 # http://mr-0xc8:55555/
