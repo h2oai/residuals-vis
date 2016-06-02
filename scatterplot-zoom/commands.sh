7a38feebf9c18041833b65667173942d954bdd47
micahstubbs <micah.stubbs@gmail.com>
2016-06-02 12:28:51 -0700

    update commands.sh with 4GB memory flag command to launch h2o-3

--- scatterplot-zoom/commands.sh
+++ scatterplot-zoom/commands.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 ssh ops@mr-0xc8
+ssh ops@172.16.2.148
 
 cd h2o-3.9.1.3469/
 
 # @micah
 java -Xmx10g -jar ./h2o.jar -port 55555 1> h2o.out 2> h2o.err &
+
+# local
+java -Xmx4g -jar ./h2o.jar -port 55555 1> h2o.out 2> h2o.err &
 
 # @markc
 # java -Xmx10g -jar ./h2o.jar -port 54321 1> h2o.out 2> h2o.err &
