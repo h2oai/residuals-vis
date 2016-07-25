d32092b8c1b9e4916c83171ac1dd4dd7bfe81a21
Micah Stubbs <micah.stubbs@gmail.com>
2016-07-25 14:46:41 -0700

    update roc-example-project

--- roc-chart-example-project/src/drawROCChart.js
+++ roc-chart-example-project/src/drawROCChart.js
@@ -1,62 +1,46 @@
 import rocChart from 'rocChart';
 import d3 from 'd3';
 
-export function drawROCChart(width, height) {
-
-  // set defaults
-  if (typeof width === 'undefined') width = 470;
-  if (typeof height === 'undefined') height = 450; 
-
-  // const margin = {top: 30, right: 61, bottom: 70, left: 61}; 
-  const margin = {top: 2, right: 2, bottom: 2, left: 2};
-  const chartWidth = width - margin.left - margin.right;
-  const chartHeight = height - margin.top - margin.bottom;
+export function drawROCChart() { 
+  const sideLength = 6.25;
+  const margin = {top: 0, right: 0, bottom: 0, left: 0};
+  const width = '100%'; // sideLength - margin.left - margin.right;
+  const height = '100%'; // sideLength - margin.top - margin.bottom;
 
   // fpr for 'false positive rate'
   // tpr for 'true positive rate'
 
   const rocChartOptions = {
-    margin: margin,
-    width: chartWidth,
-    height: chartHeight,
-    interpolationMode: 'basis',
-    // 'fpr': 'X',
-    fpr: 'fpr',
-    // 'tprVariables': [
-    //   {
-    //     'name': 'BPC',
-    //     'label': 'Break Points'
-    //   },
-    //   {
-    //     'name': 'WNR',
-    //     'label': 'Winners'
-    //   },
-    //   {
-    //     'name': 'FSP',
-    //     'label': 'First Serve %',
-    //   },
-    //   {
-    //     'name': 'NPW',
-    //     'label': 'Net Points Won'
-    //   }
-    // ],
-    tprVariables: [
+    'margin': margin,
+    'width': width,
+    'height': height,
+    'interpolationMode': 'basis',
+    'fpr': 'X',
+    'tprVariables': [
       {
-        'name': 'gbm',
-        'label': 'Gradient Boosting Machine'
+        'name': 'BPC',
+        'label': 'Break Points'
+      },
+      {
+        'name': 'WNR',
+        'label': 'Winners'
+      },
+      {
+        'name': 'FSP',
+        'label': 'First Serve %',
+      },
+      {
+        'name': 'NPW',
+        'label': 'Net Points Won'
       }
-    ],
-    animate: false,
-    smooth: true,
-    hideTicks: true,
-    hideAxes: true,
-    hideAUCText: true
+    ], 
+    'animate': true,
+    'smooth': true,
+    'hideAxes': true,
+    'hideBoundaries': true
   }
 
-  // const dataFile = 'src/data.json';
-  const dataFile = 'src/gbm-train-roc-data.json';
-
-  d3.json(dataFile, function(error, data) {
+  d3.json('src/data.json', function(error, data) {
     rocChart.plot('#root', data, rocChartOptions)
   })
 }
