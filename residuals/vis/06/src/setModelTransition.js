a7eed89fea2f67c0a35189da64ebed68b73a35ec
Micah Stubbs <micah.stubbs@gmail.com>
2016-08-16 14:03:56 -0700

    show transition for points position on model switch at once

--- residuals/vis/06/src/setModelTransition.js
+++ residuals/vis/06/src/setModelTransition.js
@@ -60,66 +60,12 @@ export function setModelTransition(selector, data, options) {
     d3.select('g.independent')
       .attr('id', currentAlgo);
 
-    // transition y-axis
-    d3.select('g.independent').select('g.axis.y')
-      .transition()
-      .delay(0)
-      .duration(1000)
-      .call(yAxis);
-
-    // transition y-axis label vertical position
-    d3.select('g.independent').select('text.y.title')
-      .transition()
-      .delay(0)
-      .duration(1000)
-      .attr('transform', `translate(${-30}, ${yScale(0)})`);
-
-    // transition x-axis vertical position
-    d3.select('g.independent').select('g.axis.x')
-      .transition()
-      .delay(0)
-      .duration(1000)
-      .attr('transform', `translate(${0}, ${yScale(0)})`);
-
     // transition marks
     d3.select('g.independent').selectAll('.marks')
       .transition()
       .delay(marksDelay)
       .duration(2000)
       .on('start', moveToNewPosition);
-
-    // transition x-axis label
-    d3.select('g.independent').select('text.x.title')
-      .transition()
-      .duration(1000)
-      .style('opacity', 0)
-      .transition()
-      .duration(0)
-      .delay(5000 + marksDelay)
-      .text(`${xVariable} (${responseVariable})`)
-      .transition()
-      .duration(1000)
-      .style('opacity', 1);
-
-    // transition model label
-    d3.select('g.independent').select('text.modelLabel')
-      .transition()
-      .duration(1000)
-      .style('opacity', 0)
-      .transition()
-      .duration(0)
-      .delay(5000 + marksDelay)
-      .text(`${currentAlgoLabel}`)
-      .transition()
-      .duration(1000)
-      .style('opacity', 0.15);
-
-    // transition x-axis
-    d3.select('g.independent').select('g.axis.x')
-      .transition()
-      .delay(2000 + marksDelay)
-      .duration(1000)
-      .call(xAxis);
 
     // set the tooltip for with new tooltipVariables
     tooltipVariables[3].name = xVariable;
@@ -157,12 +103,7 @@ export function setModelTransition(selector, data, options) {
 
     function moveToNewPosition() {
       d3.active(this)
-        .attr('cy', yScale(0))
-        .transition()
-        .delay(1000)
         .attr('cx', d => xScale(d[xVariable]))
-        .transition()
-        .delay(1000)
         .attr('cy', d => yScale(d[yVariable]));
     }   
   }
