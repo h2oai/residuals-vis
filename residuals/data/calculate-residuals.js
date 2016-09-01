b244eba75e70e82541e7b40cdd581acd5a3a2aba
Micah Stubbs <micah.stubbs@gmail.com>
2016-09-01 15:22:34 -0700

    update data processing scripts

--- residuals/data/calculate-residuals.js
+++ residuals/data/calculate-residuals.js
@@ -9,7 +9,8 @@ const rossman20kConfig = {
   predictColumn: 'predict',
   responseColumn: 'Sales',
   numericColumns: ['Customers', 'CompetitionDistance'],
-  fileSuffix: '20k'
+  fileStem: '-combined-validation-predict',
+  fileSuffix: '-20k'
 }
 
 const rossmanConfig = {
@@ -18,6 +19,7 @@ const rossmanConfig = {
   predictColumn: 'predict',
   responseColumn: 'Sales',
   numericColumns: ['Customers', 'CompetitionDistance'],
+  fileStem: '-combined-validation-predict',
   fileSuffix: ''
 }
 
@@ -28,7 +30,15 @@ const walmart20kConfig = {
   responseColumn: 'TripType',
   numericColumn: ['ScanCount'],
   residualType: 'pearson',
+  fileStem: '-combined-validation-predict',
   fileSuffix: '-20k'
+}
+
+const adultConfig = {
+  project: 'adult',
+  residualType: 'pearsonAdvisor',
+  fileStem: 'Logit',
+  fileSuffix: ''
 }
 
 function calculateResiduals(config) {
@@ -37,63 +47,63 @@ function calculateResiduals(config) {
   const responseColumn = config.responseColumn;
   const project = config.project;
   const fileSuffix = config.fileSuffix;
+  const fileStem = config.fileStem;
   const residualType = config.residualType;
 
   const inputPath = `${project}/input`;
   const outputPath = `${project}/output`;
-  const algoSpecificData = {};
+  const csvfile1 = `${inputPath}/${fileStem}${fileSuffix}.csv`;
+  const data = d3.csv.parse(fs.readFileSync(csvfile1, 'utf8'));
 
-  // loop over the algos and calcuate residuals for each one
-  algos.forEach((algo) => {
-    const csvfile1 = `${inputPath}/${algo}-combined-validation-predict${fileSuffix}.csv`;
+  const categories = d3.set(data.map(d => d[responseColumn]))
+    .values()
+    .sort(); 
 
-    const data = d3.csv.parse(fs.readFileSync(csvfile1, 'utf8'));
-    const currentMetrics = [];
+  console.log('unique categories from responseColumn', categories);
 
-    const categories = d3.set(data.map(d => d[responseColumn])).values();
-    console.log('unique categories from responseColumn', categories);
+  data.forEach((d, i) => {
+    const predicted = Number(d[predictColumn]);
+    const actual = Number(d[responseColumn]);
+    const actualString = d[responseColumn];
 
-    data.forEach((d, i) => {
-      const predicted = Number(d[predictColumn]);
-      const actual = Number(d[responseColumn]);
-      const actualString = d[responseColumn];
+    let residual;
+    if (residualType === 'pearson') {
+      const estimate = Number(d[`p${actual}`]); // p40 posterior value etc
+      const currentCategoryIndex = categories.indexOf(actualString); // '40' the category string
+      const numberOfCategories = categories.length;
+      const observed = numberOfCategories - currentCategoryIndex;
 
-      let residual;
-      if (residualType === 'pearson') {
-        const estimate = Number(d[`p${actual}`]); // p40 posterior value etc
-        const currentCategoryIndex = categories.indexOf(actualString); // '40' the category string
-        const numberOfCategories = categories.length;
-        const observed = numberOfCategories - currentCategoryIndex;
+      console.log('predicted', predicted);
+      console.log('actual', actual);
+      console.log('estimate', estimate);
+      console.log('currentCategoryIndex', currentCategoryIndex);
+      console.log('numberOfCategories', numberOfCategories);
+      console.log('observed', observed);
 
-        console.log('predicted', predicted);
-        console.log('actual', actual);
-        console.log('estimate', estimate);
-        console.log('currentCategoryIndex', currentCategoryIndex);
-        console.log('numberOfCategories', numberOfCategories);
-        console.log('observed', observed);
+      residual = (observed - estimate) / (Math.sqrt(estimate * (1 - estimate)));
+    } else if (residualType === 'pearsonAdvisor') {
+      const estimate = Number(d.Estimates);
+      const observed = Number(d.Observed);
 
-        residual = (observed - estimate) / (Math.sqrt(estimate * (1 - estimate)));
-      } else {
-        residual = actual - predicted;
-      }
-      d.residual = residual;
+      console.log('estimate', estimate);
+      console.log('observed', observed);
 
-      currentMetrics.push({});
-      currentMetrics[i][`${algo}Predict`] = predicted;
-      currentMetrics[i][`${algo}Residual`] = residual; 
-    })
-
-    algoSpecificData[`${algo}`] = currentMetrics;
-    let outputData = data;
-
-    // write a csv file
-    var writer = csvWriter();
-    writer.pipe(fs.createWriteStream(`${outputPath}/${algo}-residuals${fileSuffix}.csv`));
-    outputData.forEach(d => {
-        writer.write(d);
-    })
-    writer.end();
+      residual = (observed - estimate) / (Math.sqrt(estimate * (1 - estimate)));
+    } else {
+      residual = actual - predicted;
+    }
+    d.residual = residual;
   })
+  let outputData = data;
+
+  // write a csv file
+  var writer = csvWriter();
+  writer.pipe(fs.createWriteStream(`${outputPath}/residuals${fileSuffix}.csv`));
+  outputData.forEach(d => {
+      writer.write(d);
+  })
+  writer.end();
+
 }
 
-calculateResiduals(walmart20kConfig);
+calculateResiduals(adultConfig);
