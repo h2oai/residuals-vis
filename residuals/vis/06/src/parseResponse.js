5f624dc8f752ee3a684e44a880d2bec041ed4c04
Micah Stubbs <micah.stubbs@gmail.com>
2016-09-30 12:38:36 -0700

    ensure that enum values from API call respones are used, rather than the index for the domain in for that column in the response

--- residuals/vis/06/src/parseResponse.js
+++ residuals/vis/06/src/parseResponse.js
@@ -3,15 +3,28 @@ export function parseResponse(response) {
   const columnsData = responseData.frames[0].columns;
   const points = [];
   columnsData.forEach(d => {
+    // console.log('d.label', d.label);
+    // console.log('d from columnsData', d);
     if (Object.prototype.toString.call(d.data) === '[object Array]') {
+      // if the current column is an enum or a category
+      // recognize that the value in the data is acually a index for the
+      // domain array
+      // the value at that index position in the domain array
+      // is actually the datum that we want
       d.data.forEach((e, j) => {
         if (typeof points[j] === 'undefined') points[j] = {};
-        points[j][d.label] = e;
+          let value;
+          if (d.type === "enum" && d.domain !== null) {
+            value = d.domain[e];
+          } else {
+            value = e;
+          }
+          points[j][d.label] = value;
       });
     }
   });
-  // console.log('columnsData', columnsData);
-  // console.log('points', points);
+  console.log('columnsData', columnsData);
+  console.log('points', points);
 
   points.forEach((d, i) => {
     d.id = i;
