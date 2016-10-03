f683dad4c93f58a1c5544981806cd6ea20e95e3e
Micah Stubbs <micah.stubbs@gmail.com>
2016-10-03 11:37:06 -0700

    add progress towards react component so far

--- residuals/vis/06/webpack.config.js
+++ residuals/vis/06/webpack.config.js
@@ -1,20 +1,44 @@
 var path = require('path');
 var webpack = require('webpack');
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = {
   devtool: 'eval',
   entry: [
-    'webpack-dev-server/client?http://localhost:3333',
-    'webpack/hot/only-dev-server',
     './src/index'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/dist/'
+    library: 'residualsVis',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  externals: [
+    {
+      "react": {
+        "root": "React",
+        "commonjs2": "react",
+        "commonjs": "react",
+        "amd": "react"
+      },
+      "react-dom": {
+        "root": "ReactDOM",
+        "commonjs2": "react-dom",
+        "commonjs": "react-dom",
+        "amd": "react-dom"
+      }
+    }
+  ],
+  resolve: {
+    extensions: ['', '.js', '.css']
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new BrowserSyncPlugin({
+      host: 'localhost',
+      port: 3000,
+      server: { baseDir: ['dist'] }
+    })
   ],
   module: {
     loaders: [{
